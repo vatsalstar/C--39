9814a43341b8fee63c7fa9ba5df22502cae78213
vatsalstar <74692621+vatsalstar@users.noreply.github.com>
2021-05-05 08:32:47 +0530

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -63,6 +63,11 @@ class Game{
 
                          
                      }
+                     fill("white");
+                         textSize(25);
+                         text("Player 1:"+allPlayers.player1.score,50,50);
+                         text("Player 2:"+allPlayers.player2.score,50,100);
+                    
                     
                      
                  
@@ -104,6 +109,8 @@ class Game{
                   for (var i= 0; i < fruitGroup.length; i++) {
                       if(fruitGroup.get(i).isTouching(players)){
                           fruitGroup.get(i).destroy();
+                          player.score = player.score+1;
+                          player.update();
                       }                
                       
                   }
